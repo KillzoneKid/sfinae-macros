8fb9e5c4522d14688d8ee6ecac40f1dabb46052f
KillzoneKid <killzone_kid@hotmail.co.uk>
2018-01-05 20:40:51 +0000

    first

---
+++ README.md
@@ -0,0 +1,7 @@
+## SFINAE Macros
+
+######"Substitution Failure Is Not An Error"
+
+An example of using SFINAE macros I made, which makes enabling and disabling templates based on provided type bit more humane ;)
+
+More or different IS_XXXXXX macros could be made easily my wrapping [type_traits methods](http://en.cppreference.com/w/cpp/header/type_traits) similar those provided in the code example.
---
+++ sfinae-macros.cpp
@@ -0,0 +1,40 @@
+/*
+Example SFINAE macros and their usage
+https://ideone.com/5fVNhX
+*/
+
+#include <iostream>
+#include <type_traits>
+
+
+#define IS_SAME(T,U) std::is_same<T,U>::value
+#define IS_ARITHMETIC(T) std::is_arithmetic<T>::value
+#define ENABLE_IF(V) typename std::enable_if<V,bool>::type = true
+
+
+template <typename T, ENABLE_IF(IS_SAME(T, int) && IS_ARITHMETIC(T))>
+T foo(T input) 
+{
+	return input + 20;
+}
+
+template <typename T, ENABLE_IF(!IS_ARITHMETIC(T))>
+T foo(T input)
+{
+	return input;
+}
+
+template <typename T, ENABLE_IF(IS_SAME(T, double))>
+T foo(T input)
+{
+	return input * 1000;
+}
+
+int main() 
+{
+	std::cout << foo(0.45) << std::endl;
+	std::cout << foo(3) << std::endl;
+	std::cout << foo("sasdasd") << std::endl;
+	// std::cout << foo(true) << std::endl; <-- no template for bool, instant compiler error, nice!
+	return 0;
+}
